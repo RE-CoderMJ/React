1039438b348cb3ba19b406c74e5df2555e549cb1
RE-Coder / Minji Kim <88353455+RE-CoderMJ@users.noreply.github.com>
2023-10-09 23:00:50 +0900

    react state & working with events

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -6,13 +7,13 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.expense.date}/>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
-        <div className="expense-item__price">${props.expense.amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
-  );
+  ); 
 }
 
 export default ExpenseItem;
